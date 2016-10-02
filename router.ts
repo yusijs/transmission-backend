13849af4d4f094badaa58a4a539669932c52573f
yusiswtor <ronnie@rl-web.no>
2016-10-02 13:37:13 +0200

    Add latest routes

--- router.ts
+++ router.ts
@@ -1,13 +1,19 @@
 import express = require("express")
 
-import { router as TORRENT_ROUTER } from './routes/torrents'
-import { router as SESSION_ROUTER } from './routes/session'
+import { router as TORRENT_ROUTER } from './routes/transmission/torrents'
+import { router as SESSION_ROUTER } from './routes/transmission/session'
+
+import { router as MOVIE_ROUTER } from './routes/moviedb/movies/movies.router'
+import { router as SERIES_ROUTER } from './routes/moviedb/series/series.router'
+
+import { router as TORRENT_API } from './routes/torrent-client/torrents.router'
 
 export const router = express.Router({})
 
 router.use("/torrents", TORRENT_ROUTER)
 router.use("/session", SESSION_ROUTER)
 
-// router.get("/", (req,res) => {
-//   res.send("AsdFsdgs")
-// })
+router.use("/movies", MOVIE_ROUTER);
+router.use("/series", SERIES_ROUTER);
+
+router.use("/torrent-api", TORRENT_API);
