95e8493a33bd309c5f0a238b09e7bfe89ec1f0ee
yusijs <ronnie@rl-web.no>
2016-11-06 14:03:36 +0100

    movie by id

--- routes/moviedb/movies/movies.router.ts
+++ routes/moviedb/movies/movies.router.ts
@@ -6,3 +6,14 @@ export const router = express.Router({})
 import { router as FIND_ROUTER} from './find'
 
 router.use("/find", FIND_ROUTER)
+
+router.get("/:id", (req, res) => {
+  let {id} = req.params;
+  moviedb.movieInfo({id: id}, (err, movie) => {
+    if(err) {
+      res.status(500).send(err);
+      return;
+    }
+    res.status(200).send(movie);
+  })
+})
