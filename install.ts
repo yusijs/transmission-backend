5177b2614474085b83416600761d7affe808c1a1
yusijs <ronnie@rl-web.no>
2016-11-07 09:57:23 +0100

    install indexes

--- install.ts
+++ install.ts
@@ -8,9 +8,9 @@ const db = new sqlite3.Database(dbFile);
 
 db.parallelize(function () {
   if (!dbFileExits) {
-    db.run("CREATE TABLE Movies (id INTEGER, seen INTEGER, name TEXT, poster_path TEXT)");
-    db.run("CREATE TABLE Collections (id INTEGER, name TEXT, poster_path TEXT)");
-    db.run("CREATE TABLE Series (id INTEGER, seen INTEGER, name TEXT, poster_path TEXT)");
-    db.run("CREATE TABLE Wishlist (id INTEGER, movie INTEGER, name TEXT, poster_path TEXT)");
+    db.run("CREATE TABLE Movies (id INTEGER PRIMARY KEY, seen INTEGER, belongs_to_collection INTEGER, name TEXT, poster_path TEXT)");
+    db.run("CREATE TABLE Collections (id INTEGER PRIMARY KEY, name TEXT, poster_path TEXT)");
+    db.run("CREATE TABLE Series (id INTEGER PRIMARY KEY, seen INTEGER, belongs_to_collection INTEGER, name TEXT, poster_path TEXT)");
+    db.run("CREATE TABLE Wishlist (id INTEGER PRIMARY KEY, movie INTEGER, name TEXT, poster_path TEXT)");
   }
 });
