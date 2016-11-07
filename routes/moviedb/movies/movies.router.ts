0bca3a707dbbf20e41a9570f48562b673e2ff047
yusijs <ronnie@rl-web.no>
2016-11-07 09:57:57 +0100

    movies router - my

--- routes/moviedb/movies/movies.router.ts
+++ routes/moviedb/movies/movies.router.ts
@@ -4,8 +4,10 @@ import { moviedb } from '../../../config'
 export const router = express.Router({})
 
 import { router as FIND_ROUTER} from './find'
+import { router as MY_ROUTER } from './my';
 
 router.use("/find", FIND_ROUTER)
+router.use("/my", MY_ROUTER)
 
 router.get("/:id", (req, res) => {
   let {id} = req.params;
