9c48e917a8ffedcd8922383d68d84df3a78df665
yusijs <ronnie@rl-web.no>
2016-11-07 12:07:54 +0100

    tslint

--- router.ts
+++ router.ts
@@ -1,18 +1,18 @@
-import express = require("express")
+import express = require("express");
 
-import { router as TORRENT_ROUTER } from './routes/transmission/torrents'
-import { router as SESSION_ROUTER } from './routes/transmission/session'
+import { router as TORRENT_ROUTER } from './routes/transmission/torrents';
+import { router as SESSION_ROUTER } from './routes/transmission/session';
 
-import { router as MOVIE_ROUTER } from './routes/moviedb/movies/movies.router'
-import { router as SERIES_ROUTER } from './routes/moviedb/series/series.router'
+import { router as MOVIE_ROUTER } from './routes/moviedb/movies/movies.router';
+import { router as SERIES_ROUTER } from './routes/moviedb/series/series.router';
 import { router as COLLECTIONS_ROUTER } from './routes/moviedb/collections';
 
-import { router as TORRENT_API } from './routes/torrent-client/torrents.router'
+import { router as TORRENT_API } from './routes/torrent-client/torrents.router';
 
-export const router = express.Router({})
+export const router = express.Router({});
 
-router.use("/torrents", TORRENT_ROUTER)
-router.use("/session", SESSION_ROUTER)
+router.use("/torrents", TORRENT_ROUTER);
+router.use("/session", SESSION_ROUTER);
 
 router.use("/movies", MOVIE_ROUTER);
 router.use("/series", SERIES_ROUTER);
--- routes/transmission/torrents.ts
+++ routes/transmission/torrents.ts
@@ -1,10 +1,10 @@
 import express = require("express");
-import Transmission = require("transmission")
+import Transmission = require("transmission");
 
-import { transmission } from '../../config'
-import { router as ADD_TORRENT } from './add-torrent'
+import { transmission } from '../../config';
+import { router as ADD_TORRENT } from './add-torrent';
 
-export const router = express.Router({})
+export const router = express.Router({});
 
 router.use("/add", ADD_TORRENT);
 
@@ -12,131 +12,149 @@ router.get("/", (req, res) => {
   if (!req.query.ids) { // Get all torrents
     transmission.get((err, results) => {
       res.json(results.torrents);
-    })
+    });
   }
   else { // Get specific torrent(s)
-    let {ids} = req.query
+    let {ids} = req.query;
     // Needs to be a number, so parse the id(s)
-    if (typeof ids == 'object')
-      ids = ids.map(item => parseInt(item))
-    else
+    if (typeof ids === 'object') {
+      ids = ids.map(item => parseInt(item));
+    } else {
       ids = parseInt(ids);
+    }
     transmission.get(ids, (err, results) => {
       res.json(results.torrents);
-    })
+    });
   }
-})
+});
 
 router.put("/", (req, res) => {
   if (!req.body.ids) {
-    res.status(500).send({ status: "failed", message: "No ids specified" })
+    res.status(500).send({ status: "failed", message: "No ids specified" });
   }
   else {
-    let {ids} = req.body
+    let {ids} = req.body;
     let options: Transmission.setTorrentOptions = req.body.options;
     // Needs to be a number, so parse the id(s) just to be safe.
-    if (typeof ids == 'object')
-      ids = ids.map(item => parseInt(item))
-    else
+    if (typeof ids === 'object') {
+      ids = ids.map(item => parseInt(item));
+    } else {
       ids = parseInt(ids);
+    }
     transmission.set(ids, options, (err, results) => {
       res.json(results.torrents);
-    })
+    });
   }
-})
+});
 
 router.put("/reannounce", (req, res) => {
   if (!req.body.ids) {
-    res.status(500).send({ status: "failed", message: "No ids specified" })
+    res.status(500).send({ status: "failed", message: "No ids specified" });
   }
   else {
-    let {ids} = req.body
+    let {ids} = req.body;
     // Needs to be a number, so parse the id(s) just to be safe.
-    if (typeof ids == 'object')
-      ids = ids.map(item => parseInt(item))
-    else
+    if (typeof ids === 'object') {
+      ids = ids.map(item => parseInt(item));
+    }
+    else {
       ids = parseInt(ids);
+    }
     transmission.reannounce(ids, (err, results) => {
       res.json(results.torrents);
-    })
+    });
   }
-})
+});
 
 router.put("/start", (req, res) => {
   if (!req.body.ids) {
-    res.status(500).send({ status: "failed", message: "No ids specified" })
+    res.status(500).send({ status: "failed", message: "No ids specified" });
   }
   else {
-    let {ids} = req.body
+    let {ids} = req.body;
     // Needs to be a number, so parse the id(s) just to be safe.
-    if (typeof ids == 'object')
-      ids = ids.map(item => parseInt(item))
-    else
+    if (typeof ids === 'object') {
+      ids = ids.map(item => parseInt(item));
+    }
+    else {
       ids = parseInt(ids);
+    }
     transmission.start(ids, (err, results) => {
       res.json(results.torrents);
-    })
+    });
   }
-})
+});
 
 router.put("/startnow", (req, res) => {
   if (!req.body.ids) {
-    res.status(500).send({ status: "failed", message: "No ids specified" })
+    res.status(500).send({ status: "failed", message: "No ids specified" });
   }
   else {
-    let {ids} = req.body
+    let {ids} = req.body;
     // Needs to be a number, so parse the id(s) just to be safe.
-    if (typeof ids == 'object')
-      ids = ids.map(item => parseInt(item))
-    else
+    if (typeof ids === 'object') {
+      ids = ids.map(item => parseInt(item));
+    }
+    else {
       ids = parseInt(ids);
+    }
     transmission.startNow(ids, (err, results) => {
       res.json(results.torrents);
-    })
+    });
   }
-})
+});
 
 router.put("/stop", (req, res) => {
   if (!req.body.ids) {
-    res.status(500).send({ status: "failed", message: "No ids specified" })
+    res.status(500).send({ status: "failed", message: "No ids specified" });
   }
   else {
-    let {ids} = req.body
+    let {ids} = req.body;
     // Needs to be a number, so parse the id(s) just to be safe.
-    if (typeof ids == 'object')
-      ids = ids.map(item => parseInt(item))
-    else
+    if (typeof ids === 'object') {
+      ids = ids.map(item => parseInt(item));
+    }
+    else {
       ids = parseInt(ids);
+    }
     transmission.stop(ids, (err, results) => {
       res.json(results.torrents);
-    })
+    });
   }
-})
+});
 
 router.get("/active", (req, res) => {
   transmission.active((err, result) => {
-    if (err) res.status(500).send(err);
+    if (err) {
+      res.status(500).send(err);
+    }
     else {
       res.status(200).send(result.torrents);
     }
-  })
-})
+  });
+});
 
 router.delete("/", (req, res) => {
   if (!req.body.ids) {
-    res.status(500).send({ status: "error", message: "No ids supplied." })
+    res.status(500).send({ status: "error", message: "No ids supplied." });
     return false;
   }
   else {
     let ids = req.body.ids;
     let remove: boolean = req.body.remove ? true : false;
-    if (typeof ids == 'object')
-      ids = ids.map(item => parseInt(item))
-    else
+    if (typeof ids === 'object') {
+      ids = ids.map(item => parseInt(item));
+    }
+    else {
       ids = parseInt(ids);
+    }
     transmission.remove(ids, remove, (err, results) => {
-      if (err) res.send(err);
-      else res.send({ status: "success", message: `${ids.toString()} deleted` });
-    })
+      if (err) {
+        res.send(err);
+        return;
+      }
+      
+      res.send({ status: "success", message: `${ids.toString()} deleted` });
+    });
   }
-})
+});
