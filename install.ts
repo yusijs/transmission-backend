a263f2f282198ca74bc8e7d50866dbb491e166d1
yusijs <ronnie@rl-web.no>
2016-11-07 08:47:19 +0100

    set up sqlite db file

---
+++ install.ts
@@ -0,0 +1,16 @@
+import sqlite3 = require('sqlite3');
+import fs = require('fs');
+
+const dbFile = 'manager.db';
+const dbFileExits = fs.existsSync(dbFile);
+
+const db = new sqlite3.Database(dbFile);
+
+db.parallelize(function () {
+  if (!dbFileExits) {
+    db.run("CREATE TABLE Movies (id INTEGER, seen INTEGER, name TEXT, poster_path TEXT)");
+    db.run("CREATE TABLE Collections (id INTEGER, name TEXT, poster_path TEXT)");
+    db.run("CREATE TABLE Series (id INTEGER, seen INTEGER, name TEXT, poster_path TEXT)");
+    db.run("CREATE TABLE Wishlist (id INTEGER, movie INTEGER, name TEXT, poster_path TEXT)");
+  }
+});
