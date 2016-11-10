3e34e55ae0ce55bbf8f288dae9fac04fd123535f
yusijs <ronnie@rl-web.no>
2016-11-10 22:48:17 +0100

    discover show route

--- routes/moviedb/discovery/router.ts
+++ routes/moviedb/discovery/router.ts
@@ -1,6 +1,8 @@
 import express = require("express");
 import { router as MOVIE_ROUTER } from './movies';
+import { router as SERIES_ROUTER } from './shows';
 
 export const router = express.Router({});
 
 router.use("/movies", MOVIE_ROUTER);
+router.use("/series", SERIES_ROUTER);
