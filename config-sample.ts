b2d752d5751090f1244230f42a67c9b2ef3354a0
yusijs <ronnie@rl-web.no>
2016-11-12 14:38:44 +0100

    New sample config file

--- config-sample.ts
+++ config-sample.ts
@@ -1,17 +1,24 @@
 // Rename to config.ts
-import Transmission = require("transmission")
-import MovieDB = require("moviedb")
+import Transmission = require('transmission');
+import MovieDB = require('moviedb');
+import sqlite3 = require('sqlite3');
 
-export const moviedb = MovieDB('API_KEY')
-// Uncomment below to get api key from env variable API_KEY
-// export const moviedb = MovieDB(process.env.API_KEY)
+
+export const API_KEY = process.env.API_KEY || 'API_KEY';
+export const PASSKEY = process.env.PASSKEY || 'PASSKEY';
+export const downloadLocations = {
+  tv: '/secondary_datashare/series/ShowName/SeasonNumber',
+  movies: '/primary_datashare/movies/'
+};
 
 export const transmission = new Transmission({
   host: '127.0.0.1', // default
   port: 9091, // default
   username: "username", // default: false
   password: process.env.password // default: false
-})
+});
+
+export const moviedb = MovieDB(API_KEY);
 
 export const options = {
   method: 'POST',
@@ -24,3 +31,6 @@ export const options = {
   },
   json: true // Automatically stringifies the body to JSON 
 };
+
+const dbFile = 'db/manager.db';
+export const db = new sqlite3.Database(dbFile);
