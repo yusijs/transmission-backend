9e6c455d193e4495bd83edf70a8e284ab3e9bb2a
Ronnie Laugen <Ronnie Laugen>
2017-01-03 18:56:00 +0100

    NPM scripts for serving dev

--- public/src/app/app.config.ts
+++ public/src/app/app.config.ts
@@ -1,2 +1,2 @@
-export const API_URL = (window.location.port === '4200') ? 'http://localhost:3000/api/' : window.location.host;
+export const API_URL = (window.location.port === '4200') ? 'http://localhost:3000/api/' : '/api/';
 export const API_KEY = localStorage.getItem('api_key');
